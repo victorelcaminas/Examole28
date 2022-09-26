fa7d41bb26884ecddae64c6a590e0b97c26e02c0
rvt <alutoa2411@ieselcaminas.org>
2022-09-26 16:29:56 +0200

    first commit

---
+++ src/Example28.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Example28 {
+    public static void main(String [] args){
+        int num1, num2, i;
+        int addition = 0;
+
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        num1 = inputValue.nextInt();
+
+        System.out.println("Enter second number: ");
+        num2 = inputValue.nextInt();
+
+        for (i = 1; i <= num2; i++){
+            addition = addition + num1;
+        }
+        System.out.println(addition);
+    }
+ }
+
